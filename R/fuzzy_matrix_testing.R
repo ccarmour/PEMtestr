e116ef4543b160b7e0d1ccf67b4dec676050aa74
Claire <57771178+ccarmour@users.noreply.github.com>
2025-01-17 14:17:23 -0800

    updated to write files

--- R/fuzzy_matrix_testing.R
+++ R/fuzzy_matrix_testing.R
@@ -7,7 +7,8 @@ bgc <- "SBSmc2"
 nbrs <- read.csv("C:/Users/ccarmour.stu/OneDrive/PEM/edatopic_neighbours.csv") %>%
   as.data.frame()
 edat <- readxl::read_xlsx("C:/Users/ccarmour.stu/OneDrive/PEM/Edatopic_v12_12.xlsx") %>%
-  as.data.frame()
+  as.data.frame() %>%
+  mutate(SS_NoSpace = stringr::str_replace(SS_NoSpace, pattern = "/", replacement = "_"))
 
 # Get vector of unique site series in BGC for loop
 ss_all <- filter(edat, BGC == bgc) %>% pull(SS_NoSpace) %>% unique() %>% sort()
@@ -26,7 +27,7 @@ mtx <- matrix(data = NA_integer_,
 for(i in 1:length(ss_all)){
   # Target site series
   ss_tar <- ss_all[i]
-# ss_tar <- "SBSmc2/01"
+  # ss_tar <- "SBSmc2_01"
   # Find cells of the target site series
   ss_cells_tar <- filter(edat, SS_NoSpace == ss_tar) %>%
     pull(Edatopic) %>% sort()
@@ -49,7 +50,7 @@ for(i in 1:length(ss_all)){
   for(j in 1:length(ss_rem)){
     # Choose non-target site series from list
     ss_fuzz <- ss_rem[j]
-  # ss_fuzz <- 'SBSmc2/03'
+    # ss_fuzz <- 'SBSmc2_03'
     # Find cells of the non-target site series that may neighbour the target site series
     ss_cells_fuzz  <-  filter(edat, SS_NoSpace == ss_fuzz) %>%
       pull(Edatopic)
@@ -66,106 +67,107 @@ for(i in 1:length(ss_all)){
     mtx[ss_tar, ss_fuzz] <- score
   }
 }
-mtx
+
+write.table(as.data.frame(mtx), file = "C:/Users/ccarmour.stu/OneDrive/PEM/fuzzy_S1.txt", quote = FALSE, append = FALSE)
 
 ## SCENARIO 2
 ## Only external neighbours are counted, i.e., the neighbouring cell is not also classified as the target site series
 
 for(i in 1:length(ss_all)){
-# Target site series
-ss_tar <- ss_all[i]
-# ss_tar <- "SBSmc2/01"
-# Find cells of the target site series
-ss_cells_tar <- filter(edat, SS_NoSpace == ss_tar) %>%
-  pull(Edatopic) %>% sort()
-# Find neighbours and remove neighbours that are other target cells, e.g., external neighbours only.
-ss_nbrs <- filter(nbrs, target %in% ss_cells_tar) %>%
-  filter(., !fuzzy %in% target) %>%
-  pull(fuzzy) %>% sort()
+  # Target site series
+  ss_tar <- ss_all[i]
+  # ss_tar <- "SBSmc2/01"
+  # Find cells of the target site series
+  ss_cells_tar <- filter(edat, SS_NoSpace == ss_tar) %>%
+    pull(Edatopic) %>% sort()
+  # Find neighbours and remove neighbours that are other target cells, e.g., external neighbours only.
+  ss_nbrs <- filter(nbrs, target %in% ss_cells_tar) %>%
+    filter(., !fuzzy %in% target) %>%
+    pull(fuzzy) %>% sort()
 
-# Set score of ss_tar == ss_tar (left-right diagonal) to 1
-mtx[ss_tar, ss_tar] <- 1
+  # Set score of ss_tar == ss_tar (left-right diagonal) to 1
+  mtx[ss_tar, ss_tar] <- 1
 
-# Extract remaining site series for one-sided matrix
-# ss_rem <- setdiff(ss_all, ss_all[1:i]) %>% sort()
-# Extract remaining site series for two-sided matrix
-ss_rem <- setdiff(ss_all, ss_tar) %>% sort()
+  # Extract remaining site series for one-sided matrix
+  # ss_rem <- setdiff(ss_all, ss_all[1:i]) %>% sort()
+  # Extract remaining site series for two-sided matrix
+  ss_rem <- setdiff(ss_all, ss_tar) %>% sort()
 
-if(length(ss_rem) < 1){
-  return()
-}
+  if(length(ss_rem) < 1){
+    return()
+  }
 
-for(j in 1:length(ss_rem)){
-# Choose non-target site series from list
-ss_fuzz <- ss_rem[j]
-# ss_fuzz <- 'SBSmc2/03'
-# Find cells of the non-target site series that may neighbour the target site series
-ss_cells_fuzz  <-  filter(edat, SS_NoSpace == ss_fuzz) %>%
-  pull(Edatopic)
+  for(j in 1:length(ss_rem)){
+    # Choose non-target site series from list
+    ss_fuzz <- ss_rem[j]
+    # ss_fuzz <- 'SBSmc2/03'
+    # Find cells of the non-target site series that may neighbour the target site series
+    ss_cells_fuzz  <-  filter(edat, SS_NoSpace == ss_fuzz) %>%
+      pull(Edatopic)
 
-# Extract which target cells have overlap (share a square) with non-target cells
-sq_count <- ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz] %>% length()
-# Extract which target neighbours have overlap (share a border) with non-target cells
-br_count <- ss_nbrs[ss_nbrs %in% ss_cells_fuzz] %>% length()
+    # Extract which target cells have overlap (share a square) with non-target cells
+    sq_count <- ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz] %>% length()
+    # Extract which target neighbours have overlap (share a border) with non-target cells
+    br_count <- ss_nbrs[ss_nbrs %in% ss_cells_fuzz] %>% length()
 
-# add up the total score as per rules where shared square = +0.25 and shared border = +0.1
-score = sq_count*0.1 + br_count*0.05
+    # add up the total score as per rules where shared square = +0.25 and shared border = +0.1
+    score = sq_count*0.1 + br_count*0.05
 
-# Update the relevant cell in the matrix
-mtx[ss_tar, ss_fuzz] <- score
+    # Update the relevant cell in the matrix
+    mtx[ss_tar, ss_fuzz] <- score
   }
 }
-mtx
+write.table(as.data.frame(mtx), file = "C:/Users/ccarmour.stu/OneDrive/PEM/fuzzy_S2.txt", quote = FALSE, append = FALSE)
 
 
 ## SCENARIO 3
 ## Overlap cells are excluded entirely from boundary consideration
 
 for(i in 1:length(ss_all)){
-# Target site series
-ss_tar <- ss_all[i]
-# ss_tar <- "SBSmc2/01"
-# Find cells of the target site series
-ss_cells_tar <- filter(edat, SS_NoSpace == ss_tar) %>%
-  pull(Edatopic) %>% sort()
+  # Target site series
+  ss_tar <- ss_all[i]
+  # ss_tar <- "SBSmc2/01"
+  # Find cells of the target site series
+  ss_cells_tar <- filter(edat, SS_NoSpace == ss_tar) %>%
+    pull(Edatopic) %>% sort()
 
 
-# Set score of ss_tar == ss_tar (left-right diagonal) to 1
-mtx[ss_tar, ss_tar] <- 1
+  # Set score of ss_tar == ss_tar (left-right diagonal) to 1
+  mtx[ss_tar, ss_tar] <- 1
 
-# Extract remaining site series for one-sided matrix
-# ss_rem <- setdiff(ss_all, ss_all[1:i]) %>% sort()
-# Extract remaining site series for two-sided matrix
-ss_rem <- setdiff(ss_all, ss_tar) %>% sort()
+  # Extract remaining site series for one-sided matrix
+  # ss_rem <- setdiff(ss_all, ss_all[1:i]) %>% sort()
+  # Extract remaining site series for two-sided matrix
+  ss_rem <- setdiff(ss_all, ss_tar) %>% sort()
 
-if(length(ss_rem) < 1){
-  return()
-}
+  if(length(ss_rem) < 1){
+    return()
+  }
 
-for(j in 1:length(ss_rem)){
-# Choose non-target site series from list
-ss_fuzz <- ss_rem[j]
-# ss_fuzz <- 'SBSmc2/03'
-# Find cells of the non-target site series that may neighbour the target site series
-ss_cells_fuzz  <-  filter(edat, SS_NoSpace == ss_fuzz) %>%
-  pull(Edatopic)
+  for(j in 1:length(ss_rem)){
+    # Choose non-target site series from list
+    ss_fuzz <- ss_rem[j]
+    # ss_fuzz <- 'SBSmc2/03'
+    # Find cells of the non-target site series that may neighbour the target site series
+    ss_cells_fuzz  <-  filter(edat, SS_NoSpace == ss_fuzz) %>%
+      pull(Edatopic)
 
-# Find neighbours and remove neighbours that are other target cells AND remove the neighbours of target cells that overlap with the fuzzy cells
-ss_nbrs <- filter(nbrs, target %in% ss_cells_tar) %>%
-  filter(., !fuzzy %in% target) %>%
-  filter(., !target %in% ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz]) %>%
-  pull(fuzzy) %>% sort()
+    # Find neighbours and remove neighbours that are other target cells AND remove the neighbours of target cells that overlap with the fuzzy cells
+    ss_nbrs <- filter(nbrs, target %in% ss_cells_tar) %>%
+      filter(., !fuzzy %in% target) %>%
+      filter(., !target %in% ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz]) %>%
+      pull(fuzzy) %>% sort()
 
-# Extract which target cells have overlap (share a square) with non-target cells
-sq_count <- ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz] %>% length()
-# Extract which target neighbours have overlap (share a border) with non-target cells
-br_count <- ss_nbrs[ss_nbrs %in% ss_cells_fuzz] %>% length()
+    # Extract which target cells have overlap (share a square) with non-target cells
+    sq_count <- ss_cells_tar[ss_cells_tar %in% ss_cells_fuzz] %>% length()
+    # Extract which target neighbours have overlap (share a border) with non-target cells
+    br_count <- ss_nbrs[ss_nbrs %in% ss_cells_fuzz] %>% length()
 
-# add up the total score as per rules where shared square = +0.25 and shared border = +0.1
-score = sq_count*0.1 + br_count*0.05
+    # add up the total score as per rules where shared square = +0.25 and shared border = +0.1
+    score = sq_count*0.1 + br_count*0.05
 
-# Update the relevant cell in the matrix
-mtx[ss_tar, ss_fuzz] <- score
+    # Update the relevant cell in the matrix
+    mtx[ss_tar, ss_fuzz] <- score
   }
 }
-mtx
+write.table(as.data.frame(mtx), file = "C:/Users/ccarmour.stu/OneDrive/PEM/fuzzy_S3.txt", quote = FALSE, append = FALSE)
